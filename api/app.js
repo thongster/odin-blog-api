df66b2ee9d21ea3072c26ad0a7ed5c6ce67aca47
thongster <106802301+thongster@users.noreply.github.com>
2026-02-16 17:01:06 +0700

    setup api app.js

--- api/app.js
+++ api/app.js
@@ -1,13 +1,36 @@
+import 'dotenv/config';
+import prisma from './prisma/lib/prisma.js';
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// import routers
+const indexRouter = require('./routes/indexRouter');
+
+// passport
+const passport = require('./config/passport');
+
+// error handling
+app.use((req, res) => {
+  res.status(404).send('<h1>Error 404</h1>');
+  //   res.status(404).render("404");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('<h1>Error 500</h1>');
+  //   res.status(500).render("500");
+});
+
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Server listening on port ${PORT}!`);
 });

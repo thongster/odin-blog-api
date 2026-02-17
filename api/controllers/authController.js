5311af5517c8de991fed7c90b5a6c7d3a866aac4
thongster <106802301+thongster@users.noreply.github.com>
2026-02-17 16:01:31 +0700

    validate sign up and login

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -6,6 +6,15 @@ const bcrypt = require('bcryptjs');
 import { body, validationResult, matchedData } from 'express-validator';
 
 const validateSignUp = [
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('Username is required')
+    .isLength({ min: 3, max: 25 })
+    .withMessage('Username must be between 3 and 25 characters')
+    .matches(/^[a-zA-Z0-9_]+$/)
+    .withMessage('Username can only contain letters, numbers, and underscores'),
+
   body('email')
     .trim()
     .notEmpty()
@@ -15,34 +24,47 @@ const validateSignUp = [
     .normalizeEmail()
     .isLength({ max: 255 })
     .withMessage('Email is too long'),
+
   body('password')
+    .trim()
     .notEmpty()
     .withMessage('Password is required')
-    .isLength({ min: 6, max: 100 })
-    .withMessage('Password must be at least 6 characters'),
-  body('confirmpassword')
+    .isLength({ min: 8, max: 100 })
+    .withMessage('Password must be between 6 and 100 characters'),
+
+  body('confirmPassword')
+    .trim()
     .notEmpty()
     .withMessage('Password confirmation is required')
-    .bail()
-    .custom((value, { req }) => {
-      return value === req.body.password;
-    })
+    .custom((value, { req }) => value === req.body.password)
     .withMessage('Passwords do not match'),
-  body('firstname'),
+
+  body('firstName')
+    .trim()
+    .notEmpty()
+    .withMessage('First name is required')
+    .isLength({ min: 2, max: 25 })
+    .withMessage('First name must be between 2 and 25 characters'),
+
+  body('lastName')
+    .trim()
+    .notEmpty()
+    .withMessage('Last name is required')
+    .isLength({ min: 2, max: 25 })
+    .withMessage('Last name must be between 2 and 25 characters'),
 ];
 
 const validateLogin = [
-  body('email')
+  body('username')
     .trim()
     .notEmpty()
-    .isEmail()
-    .withMessage('Must be a valid email address')
-    .normalizeEmail()
-    .isLength({ max: 255 })
-    .withMessage('Email is too long'),
+    .withMessage('Username is required')
+    .isLength({ min: 3, max: 25 })
+    .withMessage('Username must be between 3 and 25 characters'),
+
   body('password')
     .notEmpty()
     .withMessage('Password is required')
-    .isLength({ min: 6, max: 100 })
-    .withMessage('Password must be at least 6 characters'),
+    .isLength({ min: 8, max: 100 })
+    .withMessage('Password must be between 6 and 100 characters'),
 ];

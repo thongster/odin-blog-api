6be4002adfd1fdb30f679d51810eddfb61b0f161
thongster <106802301+thongster@users.noreply.github.com>
2026-02-19 16:57:28 +0700

    create jwwt strategy

--- api/config/passport.js
+++ api/config/passport.js
@@ -2,26 +2,21 @@ import { Strategy as LocalStrategy } from 'passport-local';
 import bcrypt from 'bcryptjs';
 import passport from 'passport';
 import prisma from '../prisma/lib/prisma.js';
+import { JwtStrategy, ExtractJwt } from 'passport-local';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 passport.use(
   new LocalStrategy(
-    { usernameField: 'email' },
-    async (email, password, done) => {
+    { usernameField: 'username' },
+    async (username, password, done) => {
       try {
-        const user = await prisma.user.findUnique({ where: { email } });
+        const user = await prisma.user.findUnique({
+          where: { username: username },
+        });
 
         // if user does not exist
-        if (!user) {
-          return done(null, false, { message: 'Incorrect email' });
-        }
 
-        // password match
-        const match = await bcrypt.compare(password, user.passwordHash);
-        if (!match) {
-          return done(null, false, { message: 'Incorrect password' });
-        }
-
-        // success
         console.log('login success');
         return done(null, user);
       } catch (err) {
@@ -31,18 +26,38 @@ passport.use(
   ),
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+passport.use(
+  new JwtStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  }),
+  async (payload, done) => {
+    try {
+      const user = prisma.user.findUnique({
+        where: {
+          id: payload.id,
+        },
+      });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await prisma.user.findUnique({ where: { id } });
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  },
+);
 
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
+
+// passport.deserializeUser(async (id, done) => {
+//   try {
+//     const user = await prisma.user.findUnique({ where: { id } });
+
+//     done(null, user);
+//   } catch (err) {
+//     done(err);
+//   }
+// });
 
 export { passport };

765ac30ef752ceebdf1d4a1d84f1fd7885507a33
thongster <106802301+thongster@users.noreply.github.com>
2026-02-21 08:40:09 +0700

    layout post crud routes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -2,4 +2,19 @@ import { Router } from 'express';
 import { postsController } from '../controllers/postsController';
 const posts = Router();
 
+// view all posts
+posts.get('/posts/');
+
+// view post by id
+posts.get('/posts/:postId');
+
+// create post
+posts.post('/posts');
+
+// update post
+posts.post('/posts/:postId');
+
+// delete post
+posts.post('/posts/:postId');
+
 export { posts };

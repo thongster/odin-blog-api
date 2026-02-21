f4ee67d33699220fcaec3da9dc678d91689b395b
thongster <106802301+thongster@users.noreply.github.com>
2026-02-21 15:47:22 +0700

    laid out comments controller

--- api/controllers/commentsController.js
+++ api/controllers/commentsController.js
@@ -1,1 +1,11 @@
 const prisma = require('../lib/prisma');
+
+const getAllComments = async (req, res) => {};
+
+const createComment = async (req, res) => {};
+
+const updateComment = async (req, res) => {};
+
+const deleteComment = async (req, res) => {};
+
+export { getAllComments, createComment, updateComment, deleteComment };

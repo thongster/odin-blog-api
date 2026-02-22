4f976383c436ffd8b5b3af4e387c10457745f190
thongster <106802301+thongster@users.noreply.github.com>
2026-02-22 13:00:10 +0700

    footer mark up and styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,11 +3,20 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerInner}></div>
-      <div className={styles.credit}>
-        <p>
+      <div className={styles.inner}>
+        <p className={styles.credit}>
           View{' '}
-          <a href="https://github.com/thongster/odin-blog-api">Source Code</a>
+          <a
+            href="https://github.com/thongster/odin-blog-api"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source Code
+          </a>
+        </p>
+
+        <p className={styles.copy}>
+          © {new Date().getFullYear()} Vietnam Food Adventure
         </p>
       </div>
     </footer>

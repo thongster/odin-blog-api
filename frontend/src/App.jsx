68e2a599bfd3f98780c3d02cff22e8299ac119ef
thongster <106802301+thongster@users.noreply.github.com>
2026-02-22 09:29:17 +0700

    brainstorm pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import './styles/App.css';
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,5 +1,10 @@
 import App from '../App';
+// import Home from '../pages/Home';
+// import Profile from '../pages/Profile';
+// import CreatePost from '../pages/CreatePost';
+// import EditPost from '../pages/EditPost';
 // import Login from '../pages/Login';
+// import Signup from '../pages/Signup';
 // import ErrorPage from '../pages/ErrorPage';
 
 const routes = [

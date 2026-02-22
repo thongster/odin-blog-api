9ab5cca1ef7f4a4fc040169128d96e87d2b6d839
thongster <106802301+thongster@users.noreply.github.com>
2026-02-22 08:28:40 +0700

    set up main jsx and modern normalize

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import './styles/modern-normalize.css';
+import './index.css';
+import routes from './pages/routes.jsx';
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);

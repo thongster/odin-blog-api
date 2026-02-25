c9e991d24d71dee4c6b52ed04a9c128225231ef9
thongster <106802301+thongster@users.noreply.github.com>
2026-02-25 21:22:01 +0700

    profile summary markup

---
+++ frontend/src/components/ProfileSummary.jsx
@@ -0,0 +1,23 @@
+// import styles from './ProfileSummary.module.css';
+
+export default function ProfileSummary({ user }) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.avatar}>
+        {user.firstName[0]}
+        {user.lastName[0]}
+      </div>
+
+      <h2 className={styles.name}>
+        {user.firstName} {user.lastName}
+      </h2>
+
+      <p className={styles.username}>@{user.username}</p>
+
+      <div className={styles.info}>
+        <span>Email</span>
+        <p>{user.email}</p>
+      </div>
+    </div>
+  );
+}

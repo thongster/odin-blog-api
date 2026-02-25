98519fd7c68d5e7040cf33fd0f2b013aacb0a27a
thongster <106802301+thongster@users.noreply.github.com>
2026-02-25 15:39:54 +0700

    finish conditional rendering of nav (login, sign up, logout)

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,7 +1,38 @@
 import styles from './Nav.module.css';
 import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const Nav = () => {
+  const { token, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    logout();
+    navigate('/');
+  };
+
+  if (token) {
+    return (
+      <nav className={styles.nav}>
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) => (isActive ? styles.active : styles.link)}
+        >
+          Home
+        </NavLink>
+
+        <button
+          className={({ isActive }) => (isActive ? styles.active : styles.link)}
+          onClick={handleSignOut}
+        >
+          Signout
+        </button>
+      </nav>
+    );
+  }
+
   return (
     <nav className={styles.nav}>
       <NavLink

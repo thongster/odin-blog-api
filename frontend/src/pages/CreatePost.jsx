b8f71b0c445ce727f382e406d3e28672d4bcf33c
thongster <106802301+thongster@users.noreply.github.com>
2026-02-26 21:25:12 +0700

    write handle submit function

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,14 +1,52 @@
 import styles from './CreatePost.module.css';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const CreatePost = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [published, setPublished] = useState(false);
+  const [error, setError] = useState(null);
+  const { token, login, logout } = useAuth();
+  const navigate = useNavigate();
+  const baseUrl = 'http://localhost:3000';
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      const response = await fetch(`${baseUrl}/posts`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+
+      // logout if token expired
+      if (response.status === 401) {
+        logout();
+      }
+
+      if (!response.ok) {
+        throw new Error(
+          data.status?.[0]?.msg || data?.message || 'Signup failed',
+        );
+      }
+
+      // save jwt token
+      login(data.token);
+
+      // navigate to home page
+      navigate('/');
+      console.log('Signup successful');
+    } catch (err) {
+      setError(err.message);
+    }
   };
+
   return (
     <div className={styles.createPostPage}>
       <div className={styles.container}>

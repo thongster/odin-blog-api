3f90cf968d0be1618ddcd9fd50460137397653b0
thongster <106802301+thongster@users.noreply.github.com>
2026-03-10 17:03:22 +0700

    Set up home page components

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,7 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import './styles/App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 function App() {
   return (
--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import styles from './Home.module.css';
+import { FeaturedPost } from '../components/FeaturedPost/FeaturedPost';
+import { LatestFood } from '../components/LatestFood/LatestFood';
+import { Favorites } from '../components/Favorites/Favorites';
+import { Story } from '../components/Story/Story';
 
 const Home = () => {
   return (
-    <>
-      <div className={styles.container}></div>
-    </>
+    <div className={styles.container}>
+      <FeaturedPost />
+      <LatestFood />
+      <Favorites />
+      <Story />
+    </div>
   );
 };
 

dee031657f75a3343dfad3c3094f9ff7e63599bf
thongster <106802301+thongster@users.noreply.github.com>
2026-03-12 18:42:19 +0700

    added pages to router

--- public/src/router/router.jsx
+++ public/src/router/router.jsx
@@ -1,13 +1,21 @@
 import App from '../App';
 import { Home } from '../pages/Home';
 import ErrorPage from '../pages/ErrorPage';
+import { Adventures } from '../pages/Adventures';
+import { About } from '../pages/About';
+import { Contact } from '../pages/Contact';
 
 const routes = [
   {
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'adventures', element: <Adventures /> },
+      { path: 'about', element: <About /> },
+      { path: 'contact', element: <Contact /> },
+    ],
   },
 ];
 

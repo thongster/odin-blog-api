441845142ba095055ae5295669828f4a7cad4f0f
thongster <106802301+thongster@users.noreply.github.com>
2026-03-14 09:06:33 +0700

    pass posts and props, create useEffect so only get posts on mount

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import styles from './Home.module.css';
-import { useState } from 'react';
+import { useState, useEffect, use } from 'react';
 import { FeaturedPost } from '../components/FeaturedPost';
 import { LatestFood } from '../components/LatestFood';
 import { Favorites } from '../components/Favorites';
@@ -8,17 +8,17 @@ import { Story } from '../components/Story';
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
-  const baseUrl = import.meta.VITE_BACKEND_BASE_URL;
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 
   const getPosts = async () => {
     try {
       const response = await fetch(`${baseUrl}/posts`, {
         method: 'GET',
       });
-
       const data = await response.json();
 
       if (!response.ok) {
+        console.log('in here');
         setError(data.status?.[0]?.msg || data?.message || 'Posts not found');
         return;
       }
@@ -26,20 +26,22 @@ const Home = () => {
       console.log(`All Posts: ${data}`);
       setPosts(data);
     } catch (err) {
+      console.log('in the catch');
       setError(err.message);
     }
   };
 
-  getPosts();
-
-  // {error && <p className={styles.error}>{error}</p>}
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <div className={styles.container}>
-      <FeaturedPost />
-      <LatestFood />
-      <Favorites />
-      <Story />
+      {error && <p className={styles.error}>{error}</p>}
+      <FeaturedPost posts={posts} />
+      <LatestFood posts={posts} />
+      <Favorites posts={posts} />
+      <Story posts={posts} />
     </div>
   );
 };

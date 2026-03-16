e3e514d1f465582630cb2dd444c7624af0562d09
thongster <106802301+thongster@users.noreply.github.com>
2026-03-16 16:34:53 +0700

    created new route and page for full adventures

--- admin/src/router/router.jsx
+++ admin/src/router/router.jsx
@@ -1,24 +1,26 @@
-import App from '../App';
-import { Home } from '../pages/Home';
-import { Profile } from '../pages/Profile';
-import { CreatePost } from '../pages/CreatePost';
-import { EditPost } from '../pages/EditPost';
-import { Login } from '../pages/Login';
-import { Signup } from '../pages/Signup';
-import ErrorPage from '../pages/ErrorPage';
+import App from "../App";
+import { Home } from "../pages/Home";
+import { Profile } from "../pages/Profile";
+import { CreatePost } from "../pages/CreatePost";
+import { EditPost } from "../pages/EditPost";
+import { Login } from "../pages/Login";
+import { Signup } from "../pages/Signup";
+import { Adventures } from "../pages/Adventures";
+import ErrorPage from "../pages/ErrorPage";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'profile', element: <Profile /> },
-      { path: 'create', element: <CreatePost /> },
-      { path: 'edit/:postId', element: <EditPost /> },
-      { path: 'login', element: <Login /> },
-      { path: 'signup', element: <Signup /> },
+      { path: "profile", element: <Profile /> },
+      { path: "adventures", element: <Adventures /> },
+      { path: "create", element: <CreatePost /> },
+      { path: "edit/:postId", element: <EditPost /> },
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <Signup /> },
     ],
   },
 ];

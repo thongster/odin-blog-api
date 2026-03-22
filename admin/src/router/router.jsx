51428add3cd7811f1c5cfee39fb0b428e3371ea7
thongster <106802301+thongster@users.noreply.github.com>
2026-03-22 07:20:32 +0700

    added post page to router

--- admin/src/router/router.jsx
+++ admin/src/router/router.jsx
@@ -6,6 +6,7 @@ import { EditPost } from "../pages/EditPost";
 import { Login } from "../pages/Login";
 import { Signup } from "../pages/Signup";
 import { Adventures } from "../pages/Adventures";
+import { Post } from "../pages/Post";
 import ErrorPage from "../pages/ErrorPage";
 
 const routes = [
@@ -17,6 +18,7 @@ const routes = [
       { index: true, element: <Home /> },
       { path: "profile", element: <Profile /> },
       { path: "adventures", element: <Adventures /> },
+      { path: "post/postId", element: <Post /> },
       { path: "create", element: <CreatePost /> },
       { path: "edit/:postId", element: <EditPost /> },
       { path: "login", element: <Login /> },
